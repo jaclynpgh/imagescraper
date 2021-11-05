fc1640690f828f126920809ca50cbfa5f1b5a5c0
jaclynpgh <mannja@oregonstate.edu>
2021-11-04 22:55:55 -0400

    update

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ from bingimagescraper import image_scraper
 
 
 
-#app = Flask(__name__)
-app = Flask(__name__, static_url_path='/static')
+app = Flask(__name__)
 
 
 @app.route("/", methods=['GET'])
